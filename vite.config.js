5e7237453328690f64738763abeb63f2ec5bfe00
Bethan Helen <bethanhelen8@gmail.com>
2024-02-01 17:07:19 +0100

    upload

--- vite.config.js
+++ vite.config.js
@@ -9,9 +9,6 @@ export default{
       input:{
         main: resolve(__dirname, 'index.html'),
         movies: resolve(__dirname, 'signup/index.html'),
-        // scriptstore: resolve(__dirname, 'scriptstore/index.html'),
-        // talent: resolve(__dirname, 'talent/index.html'),
-        // contact: resolve(__dirname, 'contact/index.html'),
       }
     }
   }
